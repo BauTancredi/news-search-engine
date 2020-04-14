e5007c2db66eade310cc61963cedccd27ec1e4d5
BauTancredi <bautistatancredi@hotmail.com>
2020-04-14 09:47:47 -0300

    Form and Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <Fragment>
+      <Header title="News search engine" />
+      <div className="container white">
+        <Form />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
